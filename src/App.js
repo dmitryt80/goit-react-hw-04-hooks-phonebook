9557c71bd4a610df829cd7314f6a8de5e4f450fe
Dmitry Tartsaiev <dmitantar80@gmail.com>
2021-12-19 14:45:26 +0200

    remake to hooks

--- src/App.js
+++ src/App.js
@@ -1,77 +1,58 @@
-import React, { Component } from 'react'
-import { nanoid } from 'nanoid'
-import './App.css'
-import Container from './components/Container/Container'
-import Phonebook from './components/Phonebook/Phonebook'
-import Contacts from './components/Contacts/Contacts'
-import Filter from './components/Filter/Filter'
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import './App.css';
+import Container from './components/Container/Container';
+import Phonebook from './components/Phonebook/Phonebook';
+import Contacts from './components/Contacts/Contacts';
+import Filter from './components/Filter/Filter';
+import { getFromLS, setInLS } from "./utilits/localstorage";
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  }
-
-  componentDidMount() {
-  const getContacts = JSON.parse(localStorage.getItem("contacts"))
-  if(getContacts){
-  this.setState({contacts:getContacts})  
-  }
-  }
-
-  componentDidUpdate(_prevProps, prevState) {
-  const{contacts}=this.state
-  if(contacts!==prevState.contacts){
-  localStorage.setItem("contacts",JSON.stringify(contacts))  
-  }
-  }
-
-  onAddContact = (name, number) => {
-    if (this.onCheckContact(name)) {
-      alert(`${name} is already in contacts`)
-      return
+export default function App() {
+  const [contacts, setContacts] = useState(getFromLS("contacts"));
+  const [filter, setFilter] = useState("");
+ 
+const onCheckContact = (value) => {
+  return contacts.find((el) => el.name.toUpperCase() === value.toUpperCase())
+    ? true
+    : false;
+  };
+  
+ const onAddContact = (name, number) => {
+    if (onCheckContact(name)) {
+      alert(`${name} is already in contacts`);
+      return;
     }
-    const obj = { id: nanoid(), name, number }
-    this.setState((prevState) => ({ contacts: [...prevState.contacts, obj] }))
-  }
+    const obj = { id: nanoid(), name, number };
+    setContacts([...contacts, obj]);
+  };
 
-  onCheckContact = (value) => {
-    return this.state.contacts.find(
-      (el) => el.name.toUpperCase() === value.toUpperCase(),
-    )
-  }
+ const onDeleteContacts = (id) => {
+    setContacts(
+      contacts.filter((el) => el.id !== id));
+  };
 
-  onDeleteContacts = (id) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter((el, index) => el.id !== id),
-    }))
-  }
+  const filteringContacts = contacts.filter((el) =>
+    el.name.toUpperCase().includes(filter.toUpperCase()));
 
-  onFiltering = (value) => {
-    this.setState({ filter: value })
-  }
-
-  render() {
-    const { contacts, filter } = this.state
+  useEffect(() => {
+    setInLS("contacts", contacts);
+}, [contacts]);
+  
     return (
       <div className="App">
         <Container title="Phonebook">
-          <Phonebook onAddContact={this.onAddContact} />
+          <Phonebook onAddContact={onAddContact} />
         </Container>
         <Container title="Contacts">
           {contacts.length >= 2 && (
-            <Filter filter={filter} onFilter={this.onFiltering} />
+            <Filter filter={filter} onFilter={setFilter} />
           )}
           <Contacts
-            listContacts={contacts.filter((el) =>
-              el.name.toUpperCase().includes(filter.toUpperCase()),
-            )}
-            onDelete={this.onDeleteContacts}
+            listContacts={filteringContacts}
+            onDelete={onDeleteContacts}
           />
         </Container>
       </div>
-    )
+    );
   }
-}
 
-export default App
